c742ee1f6c9d4b7d0ed4eced626111ff26d3b83a
samir <sameer.ghag@thekhelgroup.com>
2020-03-15 01:14:05 +0530

    function components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FunctionalComponent from './components/Functional';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <FunctionalComponent />
     </div>
   );
 }
---
+++ src/components/Functional.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function FunctionalComponent() {
+    return <h1>Functional Component</h1>
+}
+
+export default FunctionalComponent;
+
+// Functional Components
+/*
+- Javascript functions 
+- optionally recieved an object of properties which is refer to as props and return HTML which describes UI
+- JSX
+*/
+
+
+// like below code we can directly exports our components
+
+// file name should be same as Function name
+
+// export - export const Functional = () => <h1>Functional Component</h1>;
+
+// import component - import {Functional} from './components/Functional';
